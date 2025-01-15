de539731b5743f48dc4d5e4a9af640cead654713
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2025-01-15 20:50:22 +0700

    g

---
+++ Lab/Test5_6.java
@@ -0,0 +1,19 @@
+package Lab;
+
+import javax.swing.*;
+
+public class Test5_6 {
+    public static void main(String[] args) {
+        int number[][] = new int[10][];
+        int count = 1;
+        int num = 1;
+        for (int i = 0; i < number.length; i++) {
+            number[i] = new int[count++];
+            for (int j = 0; j < number[i].length; j++) {
+                number[i][j] = num++;
+                System.out.print(number[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
